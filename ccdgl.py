bd2df6b2e85bc515fd7adc9fc52262ab92d42c69
Lee Trani <LMT337@gmail.com>
2018-02-22 10:06:30 -0600

    updated lauch function

--- ccdgl.py
+++ ccdgl.py
@@ -12,23 +12,27 @@ def main():
 
     group = parser.add_mutually_exclusive_group()
 
-    group.add_argument('-if', nargs=1, dest='infile', type=str, help='Input compute workflow file with all samples')
+    group.add_argument('-file', nargs=1, dest='infile', type=str, help='Input compute workflow file with all samples')
     group.add_argument('-l', help='Link to links compute workflow file', action='store_true')
 
     args = parser.parse_args()
 
     if args.l:
-        firefox_path = '/gapp/ia32linux/bin/firefox %s'
-        launch_link = 'https://imp-lims.gsc.wustl.edu/entity/compute-workflow?_show_result_set_definition=1&_result_set_name=&cw_id=&creation_event_id=&assay_id=&assay_id=&protocol_id=901&date_scheduled=&sample_full_name=&number_of_input=&number_of_instrument_data='
-        open_url = input('Open compute workflow in firefox? (y/n)\n')
-        while open_url not in ('y', 'n'):
-            open_url = input('Please enter y or n:\n')
-        if open_url == 'y':
-            print('here!!')
-            webbrowser.get(firefox_path).open(launch_link)
+        generate_compute_workflow()
 
-        print('\nCompute workflow link:\n{}'.format(launch_link))
-        print('\nCreate workflow file:\ncat > ccdg.computeworkflow.{}.tsv\n'.format(mm_dd_yy))
+def generate_compute_workflow():
+
+    firefox_path = '/gapp/ia32linux/bin/firefox %s'
+    launch_link = 'https://imp-lims.gsc.wustl.edu/entity/compute-workflow?_show_result_set_definition=1&_result_set_name=&cw_id=&creation_event_id=&assay_id=&assay_id=&protocol_id=901&date_scheduled=&sample_full_name=&number_of_input=&number_of_instrument_data='
+    open_url = input('Open compute workflow in firefox? (y/n)\n')
+    while open_url not in ('y', 'n'):
+        open_url = input('Please enter y or n:\n')
+    if open_url == 'y':
+        print('here!!')
+        webbrowser.get(firefox_path).open(launch_link)
+
+    print('\nCompute workflow link:\n{}'.format(launch_link))
+    print('\nCreate workflow file:\ncat > ccdg.computeworkflow.{}.tsv\n'.format(mm_dd_yy))
 
 
 # def sample_email:
